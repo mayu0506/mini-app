7a9017b7dfc6e14edc3366cc297fcdbc52efca61
mayu0506 <mayu.o@okadaasayuunoMacBook-Pro.local>
2019-04-15 00:30:16 +0900

    Edit all

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,19 @@
+class BlogsController < ApplicationController
+  
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+  end
+
+  def create
+    Blog.create(blog_params)
+  end
+
+  private
+  def blog_params
+    params.permit(:text)
+  end
+  
+end
